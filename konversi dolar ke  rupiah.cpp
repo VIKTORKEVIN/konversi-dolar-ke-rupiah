a768c5c6cb39daad6e2fd8fb51bd7d4feee27866
VIKTORKEVIN <46395918+VIKTORKEVIN@users.noreply.github.com>
2019-01-05 18:59:31 +0700

    Add files via upload

---
+++ konversi dolar ke  rupiah.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int dolar;
+    const float rupiah = 15000;
+
+    cout<<"----Program Konversi Dolar ke Rupiah----"<<endl;
+
+    cout<<"\n Masukkan jumlah dolar : ";
+    cin>>dolar;
+
+    //rumus menghitung konversi dolar ke rupiah
+
+    cout<<"\n Uang dolar saat ini adalah 15000/1$"<<endl;
+
+    cout<<"\n Uang yang didapat adalah="<<rupiah*dolar;
+
+    return 0;
+}
